8340780d0e5d24b907569801e77b8b73ca52ec37
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2024-09-12 08:40:57 -0300

    pylint custom_funcs.py

--- renderizador/custom_funcs.py
+++ renderizador/custom_funcs.py
@@ -1,101 +1,138 @@
-import math
+"""
+Module: custom_funcs.py
+This module contains custom functions for rendering.
+Functions:
+- fazNormal(vec): Calculates the normal vector of a given vector.
+- produtoEscalar(vec0, vec1): Calculates the dot product of two vectors.
+- L(p0, p1, p): Determines if a point is on the left side of a line.
+- dentro(p0, p1, p2, p): Determines if a point is inside a triangle.
+- diamond(px, py, x, y): Determines if a point is inside a diamond shape.
+- cameraToScreen(p, d): Converts a point from camera space to screen space.
+- rotate_quat(u, theta): Rotates a point around a given axis using quaternion rotation.
+- NDCToScreenMatrix(w, h): Generates a matrix for converting from NDC space to screen space.
+- translationMatrix(x, y, z): Generates a translation matrix.
+Note: This module also contains a main block with commented-out code for testing the functions.
+"""
+
 import numpy as np
 
+def faz_normal(vec):
+    """
+    Calculates the normal vector of a given 2D vector.
 
-def fazNormal(vec):
+    Args:
+        vec (tuple): A tuple representing a 2D vector.
+
+    Returns:
+        tuple: A tuple representing the normal vector of the input vector.
+    """
     a = vec[0]
     b = vec[1]
     return (b,-a)
 
-def produtoEscalar(vec0,vec1):
+def produto_escalar(vec0,vec1):
+    """
+    Calculates the dot product between two vectors.
+
+    Parameters:
+    vec0 (list): The first vector represented as a list of two elements.
+    vec1 (list): The second vector represented as a list of two elements.
+
+    Returns:
+    float: The dot product of the two vectors.
+    """
     return vec0[0]*vec1[0]+vec0[1]*vec1[1]
 
-def L(p0,p1,p):
+def l(p0,p1,p):
+    """
+    Determines if a point `p` is on the left side of the line segment defined by `p0` and `p1`.
+
+    Parameters:
+    - p0 (tuple): The starting point of the line segment.
+    - p1 (tuple): The ending point of the line segment.
+    - p (tuple): The point to be checked.
+
+    Returns:
+    - bool: True if the point is on the left side of the line segment, False otherwise.
+    """
     vd = (p1[0]-p0[0],p1[1]-p0[1])
     vn = (vd[1],-vd[0])
     vp = (p[0]-p0[0],p[1]-p0[1])
-    return produtoEscalar(vp,vn) >= 0
+    return produto_escalar(vp,vn) >= 0
 
 def dentro(p0,p1,p2,p):
-    l0 = L(p0,p1,p)
-    l1 = L(p1,p2,p)
-    l2 = L(p2,p0,p)
+    """
+    Determines if a point `p` is inside a triangle defined by three points `p0`, `p1`, and `p2`.
+
+    Parameters:
+    - p0: The first point of the triangle.
+    - p1: The second point of the triangle.
+    - p2: The third point of the triangle.
+    - p: The point to check.
+
+    Returns:
+    - True if the point is inside the triangle, False otherwise.
+    """
+    l0 = l(p0,p1,p)
+    l1 = l(p1,p2,p)
+    l2 = l(p2,p0,p)
     return l0 and l1 and l2
-
-def diamond(px,py,x,y):
-    within_pixel = x>=px and x<px+1 and y>=py and y<py+1
-    dec_x = x%1
-    dec_y = y%1
-    print(dec_x-dec_y>=-0.5)
-    return within_pixel and dec_x+dec_y<=1.5 and dec_x+dec_y>=0.5 and dec_x-dec_y<=0.5 and dec_x-dec_y>=-0.5
-
-def cameraToScreen(p,d = 1):
-    """  p = [x,y,z] in camera space"""
-    x = p[0]
-    y = p[1]
-    z = p[2]
-
-    u = d*x/z
-    v = d*y/z
-
-    return [u,v]
 
 
 def rotate_quat(u,theta):
-    """ p = point
+    """ 
+    Rotate a point using a quaternion rotation.
+    Parameters:
+    u (numpy.ndarray): The rotation axis.
+    theta (float): The angle of rotation.
+    Returns:
+    numpy.ndarray: The rotation matrix.
+    p = point
     u = rotation axis
-    theta = angle """
-    #u = np.array([0,0,1]) # eixo z
+    theta = angle
+    """
 
     qi = u[0][0]*np.sin(theta/2)
     qj = u[0][1]*np.sin(theta/2)
     qk = u[0][2]*np.sin(theta/2)
     qr = np.cos(theta/2)
 
-    rm = [[1-2*(qj**2+qk**2), 2*(qi*qj - qk*qr), 2*(qi*qk + qj*qr), 0],
-        [2*(qi*qj + qk*qr), 1-2*(qi**2 + qk**2), 2*(qj*qk - qi*qr), 0],
-        [2*(qi*qk - qj*qr),2*(qj*qk+qi*qr),1-2*(qi**2 + qj**2),0],
-        [0,0,0,1]]
-    
+    rm = [[1-2*(qj**2+qk**2),2*(qi*qj - qk*qr)   , 2*(qi*qk + qj*qr)     ,0],
+          [2*(qi*qj + qk*qr),1-2*(qi**2 + qk**2) , 2*(qj*qk - qi*qr)     ,0],
+          [2*(qi*qk - qj*qr),2*(qj*qk+qi*qr)     ,1-2*(qi**2 + qj**2)    ,0],
+          [0                ,0                   ,0                      ,1]]
     return rm
 
-def NDCToScreenMatrix(w,h):
-    return np.array([[w/2,0,0,w/2],
-                     [0,-h/2,0,h/2],
-                     [0,0,1,0],
-                     [0,0,0,1]])
+def NDC_to_screen_matrix(w,h):
+    """
+    Generates a matrix for converting normalized device coordinates (NDC) to screen coordinates.
 
-def translationMatrix(x,y,z):
+    Parameters:
+    w (float): The width of the screen.
+    h (float): The height of the screen.
+
+    Returns:
+    numpy.ndarray: The matrix for converting NDC to screen coordinates.
+    """
+    return np.array([[w/2, 0,  0, w/2],
+                     [0,  -h/2,0, h/2],
+                     [0,   0,  1, 0],
+                     [0,   0,  0, 1]])
+
+def translation_matrix(x,y,z):
+    """
+    Generates a translation matrix for a given translation vector (x, y, z).
+
+    Parameters:
+    x (float): The translation along the x-axis.
+    y (float): The translation along the y-axis.
+    z (float): The translation along the z-axis.
+
+    Returns:
+    numpy.ndarray: The translation matrix.
+
+    """
     return np.array([[1,0,0,x],
-                    [0,1,0,y],
-                    [0,0,1,z],
-                    [0,0,0,1]])
-
-
-
-if __name__ == "__main__":
-    """ vec = (1,2)
-    print(fazNormal(vec))
-
-    vec0 = (3,3)
-    vec1 = (0,5)
-    print(produtoEscalar(vec0,vec1))
-
-    p0 = (0,0)
-    p1 = (1,1)
-    p = (2,2)
-
-    print(L(p0,p1,p))
-
-    p0 = (0,0)
-    p1 = (0,5)
-    p2 = (5,0)
-    p = (0,0)
-
-    print(dentro(p0,p1,p2,p)) """
-
-    px = 5
-    py = 5
-    x = 5
-    y = 5.5
-    print(diamond(px,py,x,y))
+                     [0,1,0,y],
+                     [0,0,1,z],
+                     [0,0,0,1]])
